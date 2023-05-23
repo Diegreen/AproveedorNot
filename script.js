86e1b7637734920a3b3ea65b500fe4d35eba697f
diego <diegonegocios21@outlook.com>
2023-05-22 23:46:59 -0300

    att

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ function verificarAprovacao() {
     let nome = prompt("Digite o nome do aluno:");
     let presenca = parseInt(prompt("Digite a presença do aluno (0 a 10):"));
   
-    // Verificar se o nome tem menos de 5 caracteres
     if (nome.length < 5) {
       return "Nome inválido!";
     }
   
-    // Verificar se a presença é inválida
     if (presenca < 0 || presenca > 10 || isNaN(presenca)) {
       return "Presença inválida!";
     }
@@ -16,7 +14,6 @@ function verificarAprovacao() {
     for (let i = 1; i <= 10; i++) {
       let nota = parseFloat(prompt(`Digite a nota ${i} do aluno:`));
   
-      // Verificar se alguma nota é inválida
       if (nota < 0 || nota > 10 || isNaN(nota)) {
         return "Nota inválida!";
       }
@@ -24,11 +21,9 @@ function verificarAprovacao() {
       notas.push(nota);
     }
   
-    // Calcular a média das notas
     let somaNotas = notas.reduce((acc, nota) => acc + nota, 0);
     let media = somaNotas / notas.length;
   
-    // Verificar se o aluno foi aprovado ou reprovado
     if (media >= 8 && presenca >= 6) {
       return `A nota do aluno ${nome} é de ${media.toFixed(2)} e sua presença é de ${presenca}: Aluno aprovado!`;
     } else {
@@ -36,6 +31,5 @@ function verificarAprovacao() {
     }
   }
   
-  // Exemplo de uso da função
   let resultado = verificarAprovacao();
   console.log(resultado);
